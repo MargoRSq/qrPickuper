e68d0cc68f49969a0199e74d0c8522be2fd87fd6
Margo <slava1kvartovkin@gmail.com>
2021-10-05 22:30:27 +0300

    mend

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(title='instorify-api',
               version='1.0.0', docs_url="/docs")
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 @app.get("/urls/{id}")
